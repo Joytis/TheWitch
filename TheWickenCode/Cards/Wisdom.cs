4bfa1ec11c77ec1c394d17f945de12f9aa638d52
Clark Chambers <clarkchambers94@gmail.com>
2026-06-21 15:56:00 -0500

    Adding new cards holy moly

---
+++ TheWickenCode/Cards/Wisdom.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheWicken.TheWickenCode.Potions;
+
+namespace TheWicken.TheWickenCode.Cards;
+
+public sealed class Wisdom : TheWickenCard
+{
+    public Wisdom()
+        : base(0, CardType.Skill, CardRarity.Common, TargetType.Self)
+    {
+    }
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new BlockVar(5m, ValueProp.Move)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => new[]
+    {
+        HoverTipFactory.FromPotion<WickedBrew>()
+    };
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        await PotionCmd.TryToProcure<WickedBrew>(Owner);
+    }
+}
