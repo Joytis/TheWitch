bff7905b8d1b629a882a19b5bdd3b796851108b8
Clark Chambers <clarkchambers94@gmail.com>
2026-07-07 23:00:03 -0500

    Bunch of card changes - about to rename to The Witch

--- TheWickenCode/Cards/HideInABush.cs
+++ TheWickenCode/Cards/HideInABush.cs
@@ -1,29 +1,39 @@
+using BaseLib.Extensions;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using TheWicken.TheWickenCode.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
+using TheWicken.TheWickenCode.Extensions;
 
 namespace TheWicken.TheWickenCode.Cards;
 
-/// <summary>Hide in a Bush: plain, honest Block.</summary>
-public sealed class HideInABush : WickenCard
+public sealed class Spines : WickenCard
 {
-    public override bool GainsBlock => true;
+	public override bool GainsBlock => true;
 
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new BlockVar(9m, ValueProp.Move)
+	protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new BlockVar(7m, ValueProp.Move),
+        new PowerVar<BramblesPower>(3m)
     ];
 
-    public HideInABush()
-        : base(1, CardType.Skill, CardRarity.Common, TargetType.Self)
-    {
-    }
+	public Spines()
+		: base(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+	{
+	}
 
-    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
-    {
-        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block.BaseValue, ValueProp.Move, cardPlay);
-    }
+	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+	{
+		await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+		await PowerCmd.Apply<BramblesPower>(choiceContext, Owner.Creature, DynamicVars.Brambles().BaseValue, Owner.Creature, this);
+	}
 
-    protected override void OnUpgrade() => DynamicVars.Block.UpgradeValueBy(3m);
+	protected override void OnUpgrade()
+	{
+		DynamicVars.Block.UpgradeValueBy(3m);
+		DynamicVars.Brambles().UpgradeValueBy(1m);
+	}
 }
+
